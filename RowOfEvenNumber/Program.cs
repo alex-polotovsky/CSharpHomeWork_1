712de2079fc515f7bd22e481eafab4a937498306
alexandr polotovsky <apolotovsky@gmail.com>
2022-06-08 21:37:14 +0300

    RowOfEvenNumbers

---
+++ RowOfEvenNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N. */
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n <= 1)
+{
+    Console.WriteLine("Number out of range.");
+}
+else
+{
+    int EvenNumber = 2;
+
+    Console.Write("Чётные числа: ");
+
+    while (EvenNumber <= n)
+    {
+        Console.Write(" " + EvenNumber);
+        EvenNumber += 2;
+    }
+}
+
+
+
