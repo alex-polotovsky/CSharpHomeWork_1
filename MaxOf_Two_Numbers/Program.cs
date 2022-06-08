70efa57a29f6c36aaae4aaa41cbd6220cb9c0bb9
alexandr polotovsky <apolotovsky@gmail.com>
2022-06-08 19:50:48 +0300

    MaxOf_Two_Numbers

---
+++ MaxOf_Two_Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 2: Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее. */
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write($"max: {a},  min: {b}");
+}
+else
+{
+    Console.Write($"max: {b}, min: {a}");
+}
+
